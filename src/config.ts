844de23bf974209b670407e2bb4aced52528da96
Ash Huang <ash@ashsmash.com>
2022-12-29 12:46:50 -0800

    refactoring and tidying variables n stuff

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Peripress';
+export const SITE_TITLE = 'Periplus Press';
 export const SITE_DESCRIPTION = 'Periplus adventures';

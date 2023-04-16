66baa55920560fb1448bfec2113cb6f6a845feef
Ash Huang <ash@ashsmash.com>
2023-04-16 12:25:57 -0700

    trying to move site over

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://peripress.microtone.org',
+	site: 'https://peripress.github.io/callandresponse',
 	integrations: [mdx(), sitemap()],
 });
